cd3a72118249f68ebd308852ddf0308197ed537d
roliveros-ramos <ricardo.oliveros@gmail.com>
2018-07-12 03:09:38 +0200

    addRepos.R

--- addRepos.R
+++ addRepos.R
@@ -3,3 +3,6 @@ library(drat)
 options(dratBranch="master")
 
 drat::insertPackage("../mice_0.0.0.9000.tar.gz", ".")
+drat::insertPackage("../mice_0.0.0.9000.zip", ".")
+
+install.packages("mice", repo="https://roliveros-ramos.github.io/drat/")
