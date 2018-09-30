ddb53693d2291625c8dc5b941c2120de9aae4820
Ricardo Oliveros-Ramos <ricardo.oliveros@gmail.com>
2018-09-30 15:55:06 -0700

    addRepos.R

--- addRepos.R
+++ addRepos.R
@@ -5,4 +5,6 @@ options(dratBranch="master")
 drat::insertPackage("../mice_0.0.0.9000.tar.gz", ".")
 drat::insertPackage("../mice_0.0.0.9000.zip", ".")
 
-install.packages("mice", repo="https://roliveros-ramos.github.io/drat/")
+install.packages("mice", repo="https://roliveros-ramos.github.io/drat/",
+                 dependencies=TRUE)
+
